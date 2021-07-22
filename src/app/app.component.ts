bc29d43a60e26b4bfb4e9d5b526d51a45f9195fb
Akbar Odilov <akbar@odilov.de>
2021-07-22 16:09:15 +0200

    tmp + css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 
+/* structure to keep matches */
+interface IMatch{
+  value : string;
+  score : number;
+  segment : any;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'fuzzy-search';
+
+  public request : string;
+
+  public matches : IMatch[];
+
+  constructor(){
+    // initialization
+    this.request = "";
+    this.matches = [];
+  }
+
+  public search(){
+    if( !this.request ){
+      this.matches = [];
+      return;
+    }
+  }
+
 }
